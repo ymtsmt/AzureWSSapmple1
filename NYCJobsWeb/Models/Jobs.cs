617fc8697b2c1ebd72b75496cb75fe4e249834f9
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-08-24 10:19:22 +0900

    First Commit

---
+++ NYCJobsWeb/Models/Jobs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NYCJobsWeb.Models
+{
+    public class NYCJob
+    {
+        public IDictionary<string, IList<FacetResult>> Facets { get; set; }
+        public IList<SearchResult<Document>> Results { get; set; }
+        public int? Count { get; set; }
+    }
+
+    public class NYCJobLookup
+    {
+        public Document Result { get; set; }
+    }
+
+}
